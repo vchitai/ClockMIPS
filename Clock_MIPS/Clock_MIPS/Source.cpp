d3de28168659fa60c9dec120dcbbe8c52fa0a9ac
thatvinhton <thatvinh.ton@gmail.com>
2017-04-04 16:01:46 +0700

    Khoi tao interface

---
+++ Clock_MIPS/Clock_MIPS/Source.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <string.h>
+using namespace std;
+
+//Kieu luu tru : DD/MM/YYYY
+
+//Tai
+char* Date(int day, int month, int year, char* TIME) {
+
+}
+
+//Tai
+//type trong file de yeu cau
+char* Convert(char* TIME, char type) {
+
+}
+
+//Truong
+int Day(char* TIME) {
+
+}
+
+//Truong
+int Month(char* TIME) {
+
+}
+
+//Truong
+int Year(char* TIME) {
+
+}
+
+//Truong
+//Ten 2 ham khac nhau de mai mot viet MIPS phan biet giua LeapYearC char* voi LeapYearY year
+int LeapYearC(char* TIME) {
+
+}
+
+//Truong
+int LeapYearY(int year) {
+
+}
+
+//Truong
+int GetTime(char* TIME_1, char* TIME_2) {
+
+}
+
+//Truong
+//mau ket qua tra ve xem o file de bai
+char* WeekDay(char* TIME) {
+
+}
+
+//Tai
+int getStandardDay(char *TIME) {
+
+}
+
+//Vinh
+int numberOfDaysInMonth(int month) {
+
+}
+
+//ham kiem tra gi do luc nhap Vinh se them sau
+
+//Vinh
+void nhap(char *TIME) {
+
+}
+
+//Vinh
+void xuat(char *TIME) {
+
+}
+
+int main() {	
+
+	return 0;
+}
